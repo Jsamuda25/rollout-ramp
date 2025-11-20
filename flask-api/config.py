1ed61ea0943611242079dac80c917522b2689674
Jsamuda25 <jsamuda25@gmail.com>
2025-11-19 22:39:13 -0500

    Comments

--- flask-api/config.py
+++ flask-api/config.py
@@ -1,8 +1,10 @@
+# Read configuration from environment variables
 import os
 from dotenv import load_dotenv
 
-
+# Load environment variables from a .env file
 load_dotenv()
 
+# Configuration class to hold application settings
 class Config:
     IPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
